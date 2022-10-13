dc860d27a9cc3b8d5718b5080c4173841269512e
KINGVOXY <daruo0830@gmail.com>
2022-10-14 02:34:50 +0900

    script読み込み忘れを修正

--- routes/articles/[id]/status.tsx
+++ routes/articles/[id]/status.tsx
@@ -118,6 +118,9 @@ export default function Neko(props: PageProps<ArticleSchema>) {
         <BasicFooter/>
       </footer>
 
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+      <script src="/scripts/avantui.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
       <script>
         hljs.initHighlightingOnLoad();
